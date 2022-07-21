c203511259881a3abad2ad8072e5bc9bf11aeac0
misha <ggvbgg@list.ru>
2022-07-22 01:28:41 +0300

    try  add serial

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,6 @@ import java.io.Serializable;
 public class Main implements Serializable {
     public static void main(String[] args)  throws IOException, ClassNotFoundException {
         User user1 = new User();
-        user1.whiteUserObject(user1);
-        User user2 = user1.readUserObject();
-        System.out.println(user2);
+        User.whiteUserObject(user1);
     }
 }
--- src/User.java
+++ src/User.java
@@ -7,20 +7,13 @@ public class User implements Serializable {
     private static final long serialVersionUID = 1192947544117862512L;
     Scanner scan = new Scanner(System.in);
     private static int id = 1;
-    private int userId;
+    private final int userId;
     private String name;
     private String city;
     private long phoneNumber;
-    File usersDir;
+    private static File usersDir = new File("Users");
     File userFile;
-    FileOutputStream fos;
-    ObjectOutputStream oos;
-    FileInputStream fis;
-    ObjectInputStream ois;
-
-    public User(int phoneNumer) {
-        this.phoneNumber = phoneNumer;
-    }
+    public static String separator = File.separator;
 
     public User() throws IOException {
         userId = id;
@@ -28,28 +21,10 @@ public class User implements Serializable {
         setName();
         setCity();
         setPhoneNumber();
-        usersDir = new File("Users");
-        usersDir.mkdir();
-        userFile = new File(usersDir + "user" +userId + ".bin");
-        if (userFile.exists()) {
-            userFile.createNewFile();
-        }
-        oos = new ObjectOutputStream(new FileOutputStream(userFile));
-        ois = new ObjectInputStream(new FileInputStream(userFile));
     }
 
-    public void whiteUserObject (User user) throws IOException {
-        oos.writeObject(user);
-        oos.close();
-    }
-
-    public User readUserObject() throws IOException, ClassNotFoundException {
-        User user = (User) ois.readObject();
-        return user;
-    }
 
     public void setName() {
-        boolean a = true;
         System.out.print("Введите ваше имя: ");
         name = scan.nextLine();
         if (name == null || name.isEmpty()) {
@@ -61,7 +36,6 @@ public class User implements Serializable {
         ArrayList<String> cityArray = new ArrayList<>();
         File cityFile = new File("City.txt");
         BufferedReader bf = new BufferedReader(new FileReader(cityFile));
-        String nl;
         for (int i = 0; i < cityFile.length(); i++) {
             cityArray.add(bf.readLine());
         }
@@ -73,6 +47,7 @@ public class User implements Serializable {
             for (String i : cityArray) {
                 if (city.equals(i.toLowerCase(Locale.ROOT))) {
                     a = false;
+                    break;
                 }
             }
             if (a) {
@@ -105,8 +80,35 @@ public class User implements Serializable {
     public long getPhoneNumber() {
         return phoneNumber;
     }
-    
-    public String toString () {
-        return name + " " + id + " " + city + " " + phoneNumber; 
+
+    public int getId() {
+        return userId;
+    }
+
+    public String toString() {
+        return name + " " + id + " " + city + " " + phoneNumber;
+    }
+
+    public static void whiteUserObject(User user) throws IOException {
+        usersDir.mkdir();
+        File userFile = new File(usersDir + separator + "user" + user.getId() + ".bin");
+        if (userFile.exists()){
+            userFile.createNewFile();
+        }
+        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(userFile));
+        oos.writeObject(user);
+        oos.close();
+    }
+
+    public static User readUserObject(int id) {
+        usersDir.mkdir();
+        User user = null;
+        String userFileName = usersDir + separator + "user" + id + ".bin";
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(userFileName))) {
+            user = (User) ois.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return user;
     }
 }
